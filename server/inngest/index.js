7752d404b0cc6dd4b98cda917e30f0e43d54800a
HiteshKatta <hiteshkatta14@gmail>
2026-04-20 15:29:44 +0530

    fixed the bug

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,57 +1,66 @@
-import {Inngest} from "inngest";
+import { Inngest } from "inngest";
 import prisma from "../configs/prisma.js";
 
-export const inngest = new Inngest({ id: "project-management" })
+export const inngest = new Inngest({ id: "project-management" });
 
-// Ingest function to create user data in database 
+// Create user
+const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      },
+    });
+  }
+);
 
-const syncUserCreation =inngest.createFunction(
-    { id: 'sync-user-from-clerk' },
-    { event: 'clerk/user.created' },
-    async ({ event}) => {
-        const {data} = event
-        await prisma.user.create({
-            data:{
-                id: data.id,
-                email: data?.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
-            }
-        })
-    })    
+// Delete user
+const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-from-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.delete({
+      where: {
+        id: data.id,
+      },
+    });
+  }
+);
 
-// Ingest function to delete user data in database 
+// Update user
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        email: data?.email_addresses[0]?.email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      },
+    });
+  }
+);
 
-    const syncUserDeletion =inngest.createFunction(
-    { id: 'delete-user-from-clerk' },
-    { event: 'clerk/user.deleted' },
-    async ({ event}) => {
-        const {data} = event
-        await prisma.user.delete({
-            where:{
-                id: data.id,
-               
-            }
-        })
-    })  
-
-// Ingest function to update user data in database 
-
-const syncUserUpdation =inngest.createFunction(
-    { id: 'update-user-from-clerk' },
-    { event: 'clerk/user.updated' },
-    async ({ event}) => {
-        const {data} = event
-        await prisma.user.update({
-            where:{
-                id: data.id,
-            },
-            data:{
-                email: data?.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
-            }
-        })
-    })
-
-export const functions =[syncUserCreation,syncUserDeletion,syncUserUpdation];
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import { clerkMiddleware } from '@clerk/express'
 import {inngest,functions} from './inngest/index.js'
+import { serve } from 'inngest/express'
 const app = express();
 app.use(express.json())
 app.use(cors())
